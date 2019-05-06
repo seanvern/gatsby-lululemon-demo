8a629c4828bdb2c0ba77916f380e67ef532ba0e2
seanvern <svernon@lululemon.com>
2019-05-06 14:41:01 -0700

    added boilerplate

---
+++ .cache/async-requires.js
@@ -0,0 +1,10 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/Users/svernon/Projects/lulu-gatsby/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-index-js": () => import("/Users/svernon/Projects/lulu-gatsby/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+}
+
+exports.data = () => import(/* webpackChunkName: "pages-manifest" */ "/Users/svernon/Projects/lulu-gatsby/.cache/data.json")
+
