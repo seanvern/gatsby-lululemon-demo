99287424935f6a422b388db3f0f3fe0a3ff98792
seanvern <svernon@lululemon.com>
2019-05-06 15:30:34 -0700

    added setup for graphql and mdx

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { Link } from 'gatsby'
 import Layout from '../components/layout';
+import usePosts from '../hooks/use-posts';
 
-export default () =>
-  <Layout>
-    <h1>Hello lululemons!</h1>
-    <Link to="/about">Learn More About Me!</Link>
-  </Layout>;
+export default () => {
+  const posts = usePosts();
+  return (
+    <Layout>
+      <h1>Hello lululemons!</h1>
+      <Link to="/about">Learn More About Me!</Link>
+
+      <h2>Read My Blog</h2>
+      {posts.map(post => (
+        <pre>{JSON.stringify(post, null, 2)}</pre>
+      ))}
+    </Layout>
+  )
+}
