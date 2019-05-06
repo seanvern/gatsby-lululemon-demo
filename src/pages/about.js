a91f00f43a8db50efd34aaeb47653300598fcb0a
seanvern <svernon@lululemon.com>
2019-05-06 14:49:18 -0700

    added styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'gatsby'
+import Layout from '../components/layout';
 
 export default () => (
-  <>
+  <Layout>
     <h1>This is the About Page</h1>
     <p>This is the About Page</p>
     <Link to="/">Head back to Home Page</Link>
-  </>
+  </Layout>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'gatsby'
+import Layout from '../components/layout';
 
 export default () =>
-  <>
+  <Layout>
     <h1>Hello lululemons!</h1>
     <Link to="/about">Learn More About Me!</Link>
-  </>;
+  </Layout>;
